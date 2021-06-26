9155d236d628c630ffaeab0582beb62a1ede8bfc
arjunkrishna <arjun.krishna@huawei.com>
2021-06-25 20:45:45 -0400

    add initial tensorflowjs with mobilenet

---
+++ index.js
@@ -0,0 +1,10 @@
+const image = document.getElementById('img');
+
+// Load the model.
+mobilenet.load().then(model => {
+  // Classify the image.
+  model.classify(img).then(predictions => {
+    console.log('Predictions: ');
+    console.log(predictions);
+  });
+});
