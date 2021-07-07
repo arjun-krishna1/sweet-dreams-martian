3cf6be9ec6cb5dd9fe8d2e9929c79aafbaefd9b6
arjunkrishna <arjun.krishna@huawei.com>
2021-07-07 16:46:14 -0400

    set up tensorflow in web app

--- app/views.py
+++ app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+import tensorflow as tf
+
+MARS_URL_COLOR = 'https://mars.nasa.gov/msl-raw-images/msss/01590/mcam/1590MR0081020030800438E01_DXXX.jpg'
+MARS_URL_GRAY = 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/03000/opgs/edr/ncam/NLB_663825201EDR_F0850000NCAM00291M_.JPG'
 
 def index(request):
-    return render(request, 'app/index.html')
+    print("tf version")
+    print(tf.__version__)
+    context = {
+        'image_path': MARS_URL_GRAY,
+        'tf_version': tf.__version__,
+    }
+    return render(request, 'app/index.html', context)
